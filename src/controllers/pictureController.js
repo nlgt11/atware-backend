7c9ebc09c1923ebaf16368cb841743526a5377be
thuan.ngo <thuan.ngo@eastagile.com>
2021-03-21 02:03:23 +0700

    [f] Upload picture

--- src/controllers/pictureController.js
+++ src/controllers/pictureController.js
@@ -1,86 +1,92 @@
-import { Picture, UserPicture} from 'models';
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-const {JWT_KEY} = process.env;
+import { Picture, UserPicture } from 'models';
+import multer from 'multer';
+import path from 'path';
+
+const storage = multer.diskStorage({
+  destination: './public/',
+  filename: function (req, file, cb) {
+    cb(null, 'IMAGE-' + Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 },
+}).single('myfile');
+
+const uploadPic = async (req, res) => {
+  upload(req, res, async () => {
+    console.log('Request ---', req.body);
+    console.log('Request file ---', req.file); //Here you get file.
+
+    const newPic = await Picture.create({
+      userId: req.user.id,
+      url: req.file.path,
+    });
+
+    return res.send({ message: 'uploaded successfully' });
+  });
+};
 
 const getAllPictures = async (req, res) => {
   try {
-
     const pictures = await Picture.findAll({});
-    console.log(pictures)
+    console.log(pictures);
     return res.send(pictures);
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return res.status(500).send({
       error: 'Error hihi',
     });
   }
 };
 
-const getLikedPictures = async(req,res) =>{
-  try{
-    const pictures = await UserPicture.findAll({
-      where:{
-        userId: req.user.id
-      }
-    }).map(x=>x['pictureId'])
-    return res.send(pictures)
-  }
-  catch (error) {
-    console.log(error)
-    return res.status(500).send({
-      error: 'Error hihi',
-    });
-  }
-};
-const upload = async (req, res) => {
+const getLikedPictures = async (req, res) => {
   try {
-    console.log(req.body.url, req.user.id)
-    const url = req.body.url;
-    const newPicture = await Picture.create({
-      url,
-      userId: req.user.id
-    })
-    res.send(url)
+    const pictures = await UserPicture.findAll({
+      where: {
+        userId: req.user.id,
+      },
+    }).map((x) => x['pictureId']);
+    return res.send(pictures);
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return res.status(500).send({
       error: 'Error hihi',
     });
   }
 };
+
 const like = async (req, res) => {
   try {
     const img_id = req.params.id;
     console.log(img_id);
-    var isExist =  await UserPicture.findOne({
-      where:{
+    var isExist = await UserPicture.findOne({
+      where: {
         pictureId: img_id,
         userId: req.user.id,
-      }
-    })
-    if (isExist){
+      },
+    });
+    if (isExist) {
       await UserPicture.destroy({
-        where:{
+        where: {
           pictureId: img_id,
           userId: req.user.id,
-        }
-      })
-      return res.status(201).send()
-    }
-    else{
+        },
+      });
+      return res.status(201).send();
+    } else {
       const newUserPicture = await UserPicture.create({
         userId: req.user.id,
-        pictureId: img_id
-      })
-      return res.status(200).send()
+        pictureId: img_id,
+      });
+      return res.status(200).send();
     }
-    
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return res.status(500).send({
       error: 'Error hihi',
     });
   }
 };
-export {getAllPictures, getLikedPictures, upload, like};
+export { getAllPictures, getLikedPictures, uploadPic, like };
--- src/routes/picture.js
+++ src/routes/picture.js
@@ -1,12 +1,29 @@
 import express from 'express';
-import {getAllPictures, getLikedPictures ,upload, like} from '../controllers/pictureController';
+import {
+  getAllPictures,
+  getLikedPictures,
+  uploadPic,
+  like,
+} from '../controllers/pictureController';
 import auth from '../middleware/auth';
+
+// const storage = multer.diskStorage({
+//   destination: './public/',
+//   filename: function (req, file, cb) {
+//     cb(null, 'IMAGE-' + Date.now() + path.extname(file.originalname));
+//   },
+// });
+
+// const uploadImg = multer({
+//   storage: storage,
+//   limits: { fileSize: 1000000 },
+// }).single('myfile');
+
 const router = express.Router();
 
 router.get('/', auth, getAllPictures);
-router.post('/upload', auth, upload);
-router.get('/like/',auth, getLikedPictures);
-router.put('/like/:id',auth, like);
-
+router.post('/upload', auth, uploadPic);
+router.get('/like/', auth, getLikedPictures);
+router.put('/like/:id', auth, like);
 
 export default router;
